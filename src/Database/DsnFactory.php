17c7aba027399ef594a58d3cefcc8fd27553388d
Aaron Lee <sixser@foxmail.com>
2021-03-27 23:59:00 +0800

    Add postgresql support

--- src/Database/DsnFactory.php
+++ src/Database/DsnFactory.php
@@ -11,14 +11,14 @@ namespace Rush\Database;
 class DsnFactory
 {
     /**
-     * Create Mysql Tcp Connection Dsn Info
+     * Create mysql tcp connection dsn info
      * @param string $host Mysql server ip.
      * @param int $port Mysql server port.
      * @param string $dbname Database name.
      * @param string $charset Database charset.
      * @return string
      */
-    public static function createMysqlTcpDsn(string $host, int $port, string $dbname, string $charset = 'utf8'): string
+    public static function createMysqlTcpDsn(string $host, int $port, string $dbname, string $charset = 'utf8mb4'): string
     {
         return sprintf(
                 "mysql:host=%s;port=%d;dbname=%s;charset=%s",
@@ -27,17 +27,33 @@ class DsnFactory
     }
 
     /**
-     * Create Mysql Tcp Connection Dsn Info
+     * Create mysql tcp connection dsn info
      * @param string $sock Mysql sock file path.
      * @param string $dbname Database name.
      * @param string $charset Database charset.
      * @return string
      */
-    public static function createMysqlSocketDsn(string $sock, string $dbname, string $charset = 'utf8'): string
+    public static function createMysqlSocketDsn(string $sock, string $dbname, string $charset = 'utf8mb4'): string
     {
         return sprintf(
                 "mysql:unix_socket=%s;dbname=%s;charset=%s",
                 $sock, $dbname, $charset
             );
+    }
+
+    /**
+     * Create postgresql tcp connection dsn info
+     * @param string $host Postgresql server ip.
+     * @param int $port Postgresql server port.
+     * @param string $dbname Database name.
+     * @param string $charset Database charset.
+     * @return string
+     */
+    public static function createPgsqlDsn(string $host, int $port, string $dbname): string
+    {
+        return sprintf(
+            "pgsql:host=%s;port=%d;dbname=%s",
+            $host, $port, $dbname
+        );
     }
 }
